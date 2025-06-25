220b0d830c5e389e5812112d90cf36d5bbefe9d2
Kauã Vargas <166948799+KauaBinello@users.noreply.github.com>
2025-06-25 19:41:13 -0300

    Update appListeners.js

--- src/appListeners.js
+++ src/appListeners.js
@@ -90,7 +90,6 @@ function registrarListeners() {
     registrarUsuariosListeners();
     registrarLoginListeners();
     registrarJanelas();
-    registrarSessaoListeners();
     registrarDistribuicoesListeners();
     registrarMostrarAlerta();
     registrarMostrarConfirm();
